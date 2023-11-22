192b4626eb3bb796276e42b525cd4cfb67d84022
Attila Szallas <ASzallas@slb.com>
2023-11-22 16:52:25 +0100

    Add WeeklyMenuController

---
+++ Baby's Weekly Menu/Baby's Weekly Menu/Controllers/WeeklyMenuController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BabysWeeklyMenu.Controllers
+{
+    [ApiController]
+    public class WeeklyMenuController : ControllerBase
+    {
+        public JsonResult GetWeeklyMenu()
+        {
+            return new JsonResult(
+                new List<object> { 
+                    new { id = 1, Name="Fish"},
+                    new { id = 2, Name="Boiled Egg"}
+                });
+        }
+    }
+}
